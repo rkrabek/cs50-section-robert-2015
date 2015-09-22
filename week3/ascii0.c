10eed3c8b84782fcc460ad9b9c5f061831a5f204
Robert Burleigh Krabek <robertkrabek@college.harvard.edu>
2015-09-22 16:09:04 +0000

    removed solutions

---
+++ week3/ascii0.c
@@ -0,0 +1,20 @@
+/**
+ * ascii0.c - Loop through all letters
+ *
+ * Computer Science 50
+ * Week 3
+ *
+ * Eric Ouyang <eouyang@college.harvard.edu>
+ * Frederick Widjaja <fwidjaja@college.harvard.edu>
+ */
+ 
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    for (int i = 'Z'; i >= 'A'; i--)
+    {
+        printf("%c\n", i);
+    }
+}
---
+++ week3/functions2.c
@@ -0,0 +1,26 @@
+/**
+ * ascii1.c - Upper case conversion
+ *
+ * Computer Science 50
+ * Week 3
+ *
+ * Eric Ouyang <eouyang@college.harvard.edu>
+ * Frederick Widjaja <fwidjaja@college.harvard.edu>
+ */
+ 
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+// capitalize a given lower case letter
+
+int main(void)
+{
+    char str[] = "computers";
+    for (int i = 0, j = strlen(str); i < j; i++)
+    {
+        // capitalize the ith letter and replace the element in the array
+        str[i] = capitalize(str[i]);
+    }
+    printf("%s\n", str);
+}
